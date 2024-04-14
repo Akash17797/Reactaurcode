6561721c347034229d6807a8e3738371a006e82c
Akash17797 <akashdhayal31@gmail.com>
2024-04-14 15:25:34 +0530

    Changes in currency convertor

--- 06CurrencyConvertor/CurrencyConvertor/src/Hooks/useCurrencyInfo.js
+++ 06CurrencyConvertor/CurrencyConvertor/src/Hooks/useCurrencyInfo.js
@@ -12,11 +12,11 @@ function useCurrencyInfo(currency){
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then(res => res.json())
-        .then((data) => setData(data[currency]))  // in object we can access the key as object.key or the other way is object[key] aur jo setdata hai wo kaam kar rha jo change value aaegi ddata[currency] se usko store karne mei
+        .then((data) => setData(data[currency]))  // in object we can access the key as object.key or the other way is object[key] aur jo setdata hai wo kaam kar rha jo change value aaegi data[currency] se usko store karne mei
          
     }, [currency])          // useEffect(callback function, dependencies)
     console.log(data);
     return data   // jo variable humne liya data usko return kar rha hai ye
 }
 
-export default useCurrencyInfo;         // ye method pura ka pura function hi return kar deta hai
+export default useCurrencyInfo;         // ye method pura ka pura function hi return kar deta hai taaki hum isko use kar ske as a hook.
