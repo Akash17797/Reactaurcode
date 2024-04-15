f23c1c0bb61ceb3cec51bdf79136f8cf4a39a94f
Akash17797 <akashdhayal31@gmail.com>
2024-04-15 23:34:48 +0530

    store and auth reducers created

---
+++ BlogsApp/src/store/authSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status: false,
+    userData: null,
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+        
+    }
+})
+
+export const { login, logout } = authSlice.actions;  // reducers ke andar jo methods hote hai unko hum export karte hai taaki hum unko use kar sake and unko hi action kehte hai.
+
+export default authSlice.reducer;
+
+
+//jaise humne auth ka state bnaya waise hi hum post ka bhi state bna skte jo ki hmne baad mei khud se bnana hai
