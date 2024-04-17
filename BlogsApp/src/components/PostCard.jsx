1f6de15d2f87518f6e81d56650c525e0f84aa6b1
Akash17797 <akashdhayal31@gmail.com>
2024-04-17 23:09:49 +0530

    PostCard and select compoenent made

--- BlogsApp/src/components/PostCard.jsx
+++ BlogsApp/src/components/PostCard.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import appwriteService from "../appwrite/config"
+import appwriteService from "../Appwrite/Config"
 import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
-    
+    //appwrite mei id ko $id mei store karte hai. Syntax hai ye.
   return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
+                <img src={appwriteService.getFilePreview(featuredImage)} alt={title} // ye hme mila toh hmne method bnaya tha Appwrite folder mei Config ke andar
                 className='rounded-xl' />
 
             </div>
--- BlogsApp/src/components/Select.jsx
+++ BlogsApp/src/components/Select.jsx
@@ -3,7 +3,7 @@ import React, {useId} from 'react'
 function Select({
     options,
     label,
-    className,
+    className = "",
     ...props
 }, ref) {
     const id = useId()
@@ -16,14 +16,17 @@ function Select({
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         >
-            {options?.map((option) => (
+            {options?.map((option) => ( // options se hme waise toh array hi milta hai but agar koi bhi error aajaye toh usse handle karne ke liye humne map use kiya taaki array hi mile hme
+            // also agar hum directly options ko use karte toh error aata kyuki agar options null hota toh uspe map nhi chalta isliye humne ? use kiya hai taaki agar options null hota toh map na chale
                 <option key={option} value={option}>
                     {option}
                 </option>
             ))}
         </select>
-    </div>
+    </div> 
   )
 }
 
 export default React.forwardRef(Select)
+
+// Input.jsx mei hmne React.forwardRef upar use kiya tha jha function start hua tha n yha hmne neeche select kiya hai.
